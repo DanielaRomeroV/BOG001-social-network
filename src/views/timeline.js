536553da0480eb9440013e44b9d398e1be478477
unknown <dromerovarela776@gmail.com>
2020-08-20 12:43:14 -0500

    cambios 20 de agosto

--- src/views/timeline.js
+++ src/views/timeline.js
@@ -1,24 +1,12 @@
 export default () => {
   history.replaceState({}, 'timeline', '#timeline');
 
-  const header = document.querySelector('#header')
-  header.setAttribute('class', 'headerTimeline');
-  header.innerHTML = `<div class="logo"> <img src="img/icon.png"  class="logo"/> <img src="img/menu.png"  class="menu"/>
-</div>`;
-
   const timeline = document.createElement('section');
   timeline.setAttribute('class', 'containerTimeline');
-  timeline.innerHTML = `<div class="menu" id="menu-button">  <img src="img/menu.png"  class="menu"/> </div>
-  <ul class="main-menu">
-            <li class="menu-item"> <a href="">Perfil</a></li>
-            <li class="menu-item"> <a href="">Peliculas</a></li>
-            <li class="menu-item"> <a href="">Series</a></li>
-            <li class="menu-item"> <a href="">Cerrar sesión</a></li>
-        </ul>
-        <button type="submit" class="btn login">ENTRAR</button>`;
+  timeline.innerHTML= `<button type="submit" class="btn login">Publicar</button>`;
 
 //const buttonMenu = timeline.querySelector('#menu-button');
-//buttonMenu.addEventListener('click', toggleMenu);
+//buttonMenu.addEventListener('click', toggleMenu);*/
 
 
   return timeline;
