cbfc6c72d12a7fcca9f15c0adb1f480f5d7669be
unknown <dromerovarela776@gmail.com>
2020-09-01 15:30:05 -0500

    like-dislike

--- src/lib/printPost.js
+++ src/lib/printPost.js
@@ -22,7 +22,9 @@ export const printPost = (post, profile) => {
 
   const icons = document.createElement('section')
   icons.setAttribute('class', 'input-comment');
-  icons.innerHTML = `<div class="icons"><img src="img/like.png" class="likes" width="20px"/>
+  icons.innerHTML = `<div class="icons">
+  <img src="img/dislike.png" class="likes" width="20px"/>
+  <span class="likes-count">0</span>
   <img src="img/comment.png" class="commentaries" width="20px""/></div>
   <div class="inputCommentandButton"><span>
   <textarea class="inputComment" id="comment" cols="40" rows="2" required placeholder="Escribe tu comentario aquí"></textarea>
@@ -30,20 +32,39 @@ export const printPost = (post, profile) => {
   </div>`;
 
   newpost.appendChild(icons);
-   
- 
 
-  window.addEventListener('click', (e)=>{
-    if(e.target == icons.querySelector('.commentaries')){
+
+  //ocultar comentarios
+  window.addEventListener('click', (e) => {
+    if (e.target == icons.querySelector('.commentaries')) {
       icons.querySelector('.inputCommentandButton').style.display = 'block';
       newpost.querySelector('.card').style.display = 'none';
-    }else{
+    } else {
       icons.querySelector('.inputCommentandButton').style.display = 'none';
       newpost.querySelector('.card').style.display = 'block';
-    }});
-  
-    
-    return newpost;
+    }
+  });
+
+
+  //like activo inactivo
+  window.addEventListener('click', (e) => {
+    if (e.target == icons.querySelector('.likes')) {
+      if (icons.querySelector('.likes').getAttribute('src') === 'img/like.png') {
+        icons.querySelector('.likes').setAttribute('src', 'img/dislike.png');
+        icons.querySelector('.like').style.display = 'block';
+      } else if (icons.querySelector('.likes').getAttribute('src') === 'img/dislike.png') {
+        icons.querySelector('.likes').setAttribute('src', 'img/like.png');
+        icons.querySelector('.like').style.display = 'block';
+      }
+    }
+  });
+
+
+
+
+
+
+  return newpost;
 };
 
 
@@ -57,12 +78,12 @@ export const printPost = (post, userName) => {
   newpost.innerHTML = `<div class="card">
   <div class="content">
   <div class="header">
-    <div class="profile-pic"><img src="${currentUser().photoURL}" id="profile-pic"/></div>        
+    <div class="profile-pic"><img src="${currentUser().photoURL}" id="profile-pic"/></div>
       <div class="detail">
       <p class="name">${userName.name}</p>
-      <p class="posted">${Date.toDate}</p>          
-    </div>   
-    <div class="categories"></div>   
+      <p class="posted">${Date.toDate}</p>
+    </div>
+    <div class="categories"></div>
   </div>
   <div class="desc">
   ${post.comment}
@@ -76,11 +97,11 @@ export const printPost = (post, userName) => {
     <textarea class="inputComment" id="comment" cols="40" rows="2" required placeholder="Escribe tu comentario aquí"></textarea>
     <button type="submit" class="btnCommentaries">Enviar</button>
     </div>`;
-  
+
     newpost.appendChild(icons);
-     
+
     //commentPublish(comment, category, userID);
-   
+
     icons.querySelector('.commentaries').addEventListener('click', () => {
     icons.querySelector('.inputCommentandButton').style.display = "block";
     });
@@ -91,8 +112,7 @@ export const printPost = (post, userName) => {
       }else{
         icons.querySelector('.inputCommentandButton').style.display = 'none';
       });
-      
+
       return newpost;
 };
 */
-      
