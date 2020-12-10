737592633dee44acc50ffbb9057ddcdaa1a2f0fc
unknown <dromerovarela776@gmail.com>
2020-12-09 23:52:32 -0500

    helivisuel

--- components/modal.js
+++ components/modal.js
@@ -2,20 +2,12 @@ import { commentPublish } from '../lib/firebaseFirestore.js';
 
 export const publish = (userPhoto, userID, userName) => {
   const photoDefault = 'https://www.nicepng.com/png/detail/202-2022264_usuario-annimo-usuario-annimo-user-icon-png-transparent.png';
-<<<<<<< HEAD
-
-=======
->>>>>>> 46b62a711834c0f798572c147c3184d83eb9f01a
   const modal = document.createElement('section');
   modal.setAttribute('class', 'modal');
   modal.innerHTML = `<div class="modal-content">
         <header id="ModalHeader">
           <span class="close">&times;</span>
-<<<<<<< HEAD
-          <img id="userPhoto" src="" >
-=======
           <img id="userPhoto" src=" " >
->>>>>>> 46b62a711834c0f798572c147c3184d83eb9f01a
           <p>Comenta sobre tus peliculas o series favoritas</p>
         </header>
         <form id='formComment' enctype="multipart/form-data">
@@ -39,10 +31,6 @@ export const publish = (userPhoto, userID, userName) => {
   photo.src = `${(userPhoto === null ? photoDefault : userPhoto)}`;
   const publish = modal.querySelector('#formComment');
 
-<<<<<<< HEAD
-  // const img = form.loadImg.value;
-=======
->>>>>>> 46b62a711834c0f798572c147c3184d83eb9f01a
   publish.addEventListener('submit', (e) => {
     e.preventDefault();
     const comment = publish.comment.value;
